650f2d22dbd4e4cae6bcad53ac3540e4f2250b7c
YSS in the BBBlab <35651143+HANYOUHA@users.noreply.github.com>
2024-01-17 00:12:44 +0900

    First Commit

---
+++ test.py
@@ -0,0 +1,41 @@
+import sys
+
+import pygame
+
+from button import button
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280,720))
+pygame.display.set_caption('main menu')
+
+BG = pygame.image.load('대충 배경 사진')
+
+def get_font(size):
+
+def main_menu():
+    pygame.display.set_caption('Main Menu')
+
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render
+
+
+def textbox_test():
+    pygame.display.set_caption('textbox test')
+
+    while True:
+
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill('black')
+
+        #PLAY_TEXT 구현
+
+
+def keyboard_test():
+
+
